17a3bd7b0ea51cd9106c8e5e540cae590c8d7e74
AkshatShetty101 <akshatshetty2908@gmail.com>
2018-03-19 17:13:36 +0530

    Modularised and documented

--- server/main/src/routes/blockchain/addForm.js
+++ server/main/src/routes/blockchain/addForm.js
@@ -1,58 +1,74 @@
+// Declaring constants
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
 const config = require('config');
 
 // Declaring block-chain related information 
 this.bizNetworkConnection = new BusinessNetworkConnection();
 this.cardName = config.get('cardName');
-this.businessNetworkIdentifier = 'aics.bna';
+this.businessNetworkIdentifier = config.get('bna');
 this.NS = config.get('NS');
 this.NS_F = config.get('NS_F');
 this.NS_P = config.get('NS_P');
 
 module.exports = (req, res, next) => {
-    //Establishing connection
+    // Establishing connection
     this.bizNetworkConnection.connect(this.cardName)
         .then((result) => {
             this.businessNetworkDefinition = result;
+            // Getting factory definitions
             let factory = this.businessNetworkDefinition.getFactory();
+            // Loading participant registry
             this.bizNetworkConnection.getParticipantRegistry(this.NS_P)
                 .then((personRegistry) => {
                     console.log(req.body);
+                    // Verifying if person exists
                     personRegistry.exists(req.body.personId).then((persons) => {
+                        // Loading the form registry
                         this.bizNetworkConnection.getAssetRegistry(this.NS_F)
                             .then((formRegistry) => {
+                                // Creating relationship for person
                                 let asignee = factory.newRelationship(this.NS, 'Person', req.body.personId);
+                                // Creating a new form asset
                                 form = factory.newResource(this.NS, 'Form', req.body.formId);
                                 form.assigneeId = [asignee];
                                 form.isValid = true;
+                                // Adding form to form registry
                                 formRegistry.add(form).then((data) => {
-                                    let transaction = factory.newTransaction(this.NS,'Event');
-                                    transaction.person=ob;
-                                    transaction.type="template_created";
-                                    if(req.body.metadata)
-                                    transaction.metadata=JSON.stringify(req.body.metadata);
+                                    // Creating the transaction
+                                    let transaction = factory.newTransaction(this.NS, 'Event');
+                                    transaction.person = ob;
+                                    transaction.type = "template_created";
+                                    if (req.body.metadata)
+                                        transaction.metadata = JSON.stringify(req.body.metadata);
                                     else
-                                    transaction.metadata="{}";
+                                        transaction.metadata = "{}";
                                     transaction.form = factory.newRelationship(this.NS, 'Form', req.body.formId);
-                                    this.bizNetworkConnection.submitTransaction(transaction).then((result)=>{
+                                    // Submitting the transaction
+                                    this.bizNetworkConnection.submitTransaction(transaction).then((result) => {
                                         console.log(result);
+                                        // Returning response
                                         res.json({ "status": 1, "message": "Form Added successfully" });
                                     });
                                 }).catch((err) => {
+                                    // Catching errors
                                     console.log(err.message);
                                     res.send({ "status": -1, "message": err.message });
                                 });
                             }).catch((err) => {
+                                // Catching errors
                                 console.log(err.message);
                                 res.send({ "status": -1, "message": err.message });
                             });
                     }).catch((err) => {
+                        // Catching errors
                         console.log(err.message);
                         res.send({ "status": -1, "message": err.message });
                     });
                 });
         })
         .catch(err => {
+            // Catching errors
             console.log(err);
+            res.send({ "status": -1, "message": err.message });
         });
 }
--- server/main/src/routes/blockchain/addPerson.js
+++ server/main/src/routes/blockchain/addPerson.js
@@ -1,29 +1,40 @@
+// Declaring constants
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
 const config = require('config');
 
+// Declaring block-chain related information 
 this.bizNetworkConnection = new BusinessNetworkConnection();
 this.cardName = config.get('cardName');
-this.businessNetworkIdentifier = 'aics.bna';
+this.businessNetworkIdentifier = config.get('bna');;
 
 module.exports = (req, res, next) => {
+    // Establishing connection
     this.bizNetworkConnection.connect(this.cardName)
         .then((result) => {
             this.businessNetworkDefinition = result;
+            // Getting factory definitions
             let factory = this.businessNetworkDefinition.getFactory();
+            // Loading participant registry
             this.bizNetworkConnection.getParticipantRegistry('org.acme.aics.Person')
                 .then((PersonRegistry) => {
                     console.log(req.body);
+                    // Creating a new participant person
                     person = factory.newResource('org.acme.aics', 'Person', req.body.personId);
                     person.designation = req.body.designation;
+                    // Adding form to form registry
                     PersonRegistry.add(person).then((data) => {
-                        res.send({"status":1,"message":req.body.designation+" added successfully"});
-                    }).catch((err)=>{
+                        // Returning response
+                        res.send({ "status": 1, "message": req.body.designation + " added successfully" });
+                    }).catch((err) => {
+                        // Catching errors
                         console.log(err.message);
-                        res.send({"status":-1,"message":err.message});
+                        res.send({ "status": -1, "message": err.message });
                     });
                 });
         })
         .catch(err => {
+            // Catching errors
             console.log(err);
+            res.send({ "status": -1, "message": err.message });
         });
 }
