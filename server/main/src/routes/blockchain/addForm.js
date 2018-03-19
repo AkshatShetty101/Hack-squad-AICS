c12601235a68de31b8f7ca61cde2e18335452f25
AkshatShetty101 <akshatshetty2908@gmail.com>
2018-03-19 19:39:49 +0530

    Cleaning up

--- server/main/src/routes/blockchain/addForm.js
+++ server/main/src/routes/blockchain/addForm.js
@@ -2,7 +2,7 @@
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
 const config = require('config');
 
-// Declaring block-chain related information 
+// Declaring block-chain related information
 this.bizNetworkConnection = new BusinessNetworkConnection();
 this.cardName = config.get('cardName');
 this.businessNetworkIdentifier = config.get('bna');
@@ -11,70 +11,70 @@ this.NS_F = config.get('NS_F');
 this.NS_P = config.get('NS_P');
 
 module.exports = (req, res) => {
-    // Establishing connection
-    this.bizNetworkConnection.connect(this.cardName)
-        .then((result) => {
-            this.businessNetworkDefinition = result;
-            // Getting factory definitions
-            let factory = this.businessNetworkDefinition.getFactory();
-            // Loading participant registry
-            this.bizNetworkConnection.getParticipantRegistry(this.NS_P)
-                .then((personRegistry) => {
-                    console.log(req.body);
-                    // Verifying if person exists
-                    personRegistry.exists(req.body.personId).then((persons) => {
-                        // Loading the form registry
-                        this.bizNetworkConnection.getAssetRegistry(this.NS_F)
-                            .then((formRegistry) => {
-                                // Creating relationship for person
-                                let asignee = factory.newRelationship(this.NS, 'Person', req.body.personId);
-                                // Creating a new form asset
-                                form = factory.newResource(this.NS, 'Form', req.body.formId);
-                                form.assigneeId = [asignee];
-                                form.isValid = true;
-                                // Adding form to form registry
-                                console.log("Reached here!");
-                                formRegistry.add(form).then((data) => {
-                                    // Creating the transaction
-                                    console.log("Reached here!");
-                                    let transaction = factory.newTransaction(this.NS, 'Event');
-                                    console.log("Reached here!");
-                                    transaction.person = asignee;
-                                    transaction.type = "template_created";
-                                    if (req.body.metadata)
-                                        transaction.metadata = JSON.stringify(req.body.metadata);
-                                    else
-                                        transaction.metadata = "{}";
-                                    console.log("Reached here!");
-                                    transaction.form = factory.newRelationship(this.NS, 'Form', req.body.formId);
-                                    // Submitting the transaction
-                                    console.log("Reached here!");
-                                    this.bizNetworkConnection.submitTransaction(transaction).then((result) => {
-                                        console.log(result);
-                                        // Returning response
-                                        console.log("Reached here!");
-                                        res.json({ "success": true, "message": "Form Added successfully" });
-                                    });
-                                }).catch((err) => {
-                                    // Catching errors
-                                    console.log(err.message);
-                                    res.send({ "success": false, "message": err.message });
-                                });
-                            }).catch((err) => {
-                                // Catching errors
-                                console.log(err.message);
-                                res.send({ "success": false, "message": err.message });
-                            });
-                    }).catch((err) => {
-                        // Catching errors
-                        console.log(err.message);
-                        res.send({ "success": false, "message": err.message });
-                    });
-                });
-        })
-        .catch(err => {
-            // Catching errors
-            console.log(err);
-            res.send({ "success": false, "message": err.message });
-        });
+	// Establishing connection
+	this.bizNetworkConnection.connect(this.cardName)
+		.then((result) => {
+			this.businessNetworkDefinition = result;
+			// Getting factory definitions
+			let factory = this.businessNetworkDefinition.getFactory();
+			// Loading participant registry
+			this.bizNetworkConnection.getParticipantRegistry(this.NS_P)
+				.then((personRegistry) => {
+					console.log(req.body);
+					// Verifying if person exists
+					personRegistry.exists(req.body.personId).then((persons) => {
+						// Loading the form registry
+						this.bizNetworkConnection.getAssetRegistry(this.NS_F)
+							.then((formRegistry) => {
+								// Creating relationship for person
+								let asignee = factory.newRelationship(this.NS, 'Person', req.body.personId);
+								// Creating a new form asset
+								form = factory.newResource(this.NS, 'Form', req.body.formId);
+								form.assigneeId = [asignee];
+								form.isValid = true;
+								// Adding form to form registry
+								console.log("Reached here!");
+								formRegistry.add(form).then((data) => {
+									// Creating the transaction
+									console.log("Reached here!");
+									let transaction = factory.newTransaction(this.NS, 'Event');
+									console.log("Reached here!");
+									transaction.person = asignee;
+									transaction.type = "template_created";
+									if (req.body.metadata)
+										transaction.metadata = JSON.stringify(req.body.metadata);
+									else
+										transaction.metadata = "{}";
+									console.log("Reached here!");
+									transaction.form = factory.newRelationship(this.NS, 'Form', req.body.formId);
+									// Submitting the transaction
+									console.log("Reached here!");
+									this.bizNetworkConnection.submitTransaction(transaction).then((result) => {
+										console.log(result);
+										// Returning response
+										console.log("Reached here!");
+										res.json({ 'success': true, 'message': 'Form Added successfully' });
+									});
+								}).catch((err) => {
+									// Catching errors
+									console.log(err.message);
+									res.send({ 'success': false, 'message': err.message });
+								});
+							}).catch((err) => {
+								// Catching errors
+								console.log(err.message);
+								res.send({ 'success': false, 'message': err.message });
+							});
+					}).catch((err) => {
+						// Catching errors
+						console.log(err.message);
+						res.send({ 'success': false, 'message': err.message });
+					});
+				});
+		})
+		.catch(err => {
+			// Catching errors
+			console.log(err);
+			res.send({ 'success': false, 'message': err.message });
+		});
 }
--- server/main/src/routes/users/addPerson.js
+++ server/main/src/routes/users/addPerson.js
@@ -3,36 +3,36 @@ const divison = require('../../models/division');
 const uuid4 = require('uuid/v4');
 const passwordGenerator = require('../../utils/passwordGenerator');
 module.exports = (req, res, next) => {
-    //Some db related work
-    divison.findOne({ name: req.body.division_name }, { divison_id: 1 }, function (err, divisionData) {
-        console.log(divisionData);
-        if (err) {
-            res.send({ success: false, message: err });
-        } else if (divisionData) {
-            let password = passwordGenerator.hash(req.body.password);
-            var userData = new person({
-                user_id: uuid4(),
-                name: {
-                    firstName: req.body.firstname,
-                    middleName: req.body.middleName ? req.body.middleName : '',
-                    lastName: req.body.lastName
-                },
-                access_form_id: [],
-                password: password,
-                email: req.body.email,
-                divison_id: divisionData.divison_id,
-                designation: req.body.designation
-            });
-            userData.save(function (err, result) {
-                if (err) {
-                    res.send({ success: false, message: err });
-                } else {
-                    next();
-                }
-            });
-            next();
-        } else {
-            res.send({ success: false, message: "No such Division/Group/Organization" });
-        }
-    });
+	//Some db related work
+	divison.findOne({ name: req.body.division_name }, { divison_id: 1 }, function (err, divisionData) {
+		console.log(divisionData);
+		if (err) {
+			res.send({ success: false, message: err });
+		} else if (divisionData) {
+			let password = passwordGenerator.hash(req.body.password);
+			var userData = new person({
+				user_id: uuid4(),
+				name: {
+					firstName: req.body.firstname,
+					middleName: req.body.middleName ? req.body.middleName : '',
+					lastName: req.body.lastName
+				},
+				access_form_id: [],
+				password: password,
+				email: req.body.email,
+				divison_id: divisionData.divison_id,
+				designation: req.body.designation
+			});
+			userData.save(function (err) {
+				if (err) {
+					res.send({ success: false, message: err });
+				} else {
+					next();
+				}
+			});
+			next();
+		} else {
+			res.send({ success: false, message: 'No such Division/Group/Organization' });
+		}
+	});
 }
