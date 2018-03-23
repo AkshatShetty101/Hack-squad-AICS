23edd50d758edb7fbabdb6e1b694967fcd224001
Nisheet Sinvhal <nisheet1.sinvhal@gmail.com>
2018-03-23 12:46:50 +0530

    Minor enhancements

--- server/main/src/routes/templates/addTemplate.js
+++ server/main/src/routes/templates/addTemplate.js
@@ -3,10 +3,10 @@ const Template = require('../../models/template');
 
 module.exports = (req, res, next) => {
 	// Setting data for new template
-	if (res.locals.user._id && req.body.tags && req.body.format && res.locals.requestId && req.body.title) {
+	if (req.body.format && req.body.title) {
 		const templateData = new Template({
 			created_by: res.locals.user._id,
-			tags: req.body.tags,
+			tags: req.body.tags ? req.body.tags : [],
 			format: req.body.format,
 			title: req.body.title
 		});
