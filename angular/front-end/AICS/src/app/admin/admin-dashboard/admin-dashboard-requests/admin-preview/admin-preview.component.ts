a61c3ee55be80531e55cf07ce56b27c1041119da
Akshat Shetty <akshatshetty2908@gmail.com>
2018-03-26 02:19:02 +0530

    finished admin due requests

--- angular/front-end/AICS/src/app/admin/admin-dashboard/admin-dashboard-requests/admin-preview/admin-preview.component.ts
+++ angular/front-end/AICS/src/app/admin/admin-dashboard/admin-dashboard-requests/admin-preview/admin-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-admin-preview',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-preview.component.scss']
 })
 export class AdminPreviewComponent implements OnInit {
-
+  @Input() data :{id:string,title:string,data:string}={id:"default",data:"default",title:"default"};
   constructor() { }
 
   ngOnInit() {
