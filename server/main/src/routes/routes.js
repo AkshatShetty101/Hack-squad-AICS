c3e8020e44babec0e222530d84277f193cab0feb
Akshat Shetty <akshatshetty2908@gmail.com>
2018-03-25 11:37:16 +0530

    Working

--- server/main/src/routes/routes.js
+++ server/main/src/routes/routes.js
@@ -160,6 +160,14 @@ router.post('/forms/approveAdmin',
 	require('./blockchain/approveForm'),
 	require('./blockchain/submitForm'));
 
+router.post('/forms/rejectAdminAndImprove',
+	verifyMiddleware.verifyPerson,
+	verifyMiddleware.verifyAdmin,
+	require('./forms/rejectFormAdmin'),
+	require('./blockchain/rejectForm'),
+	require('./forms/assignGCAndDeadline'),
+	require('./blockchain/assignForm'))
+
 router.post('/forms/approveRA',
 	verifyMiddleware.verifyPerson,
 	verifyMiddleware.verifyRequestingAuthority,
