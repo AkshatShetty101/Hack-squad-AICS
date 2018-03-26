5adacd284c1b49c142be507df7c2329f89ac7fb9
Akshat Shetty <akshatshetty2908@gmail.com>
2018-03-26 13:52:37 +0530

    More work

---
+++ angular/front-end/AICS/src/app/admin/admin-dashboard/admin-dashboard-verifications/admin-verifications-list/admin-verifications-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-verifications-list',
+  templateUrl: './admin-verifications-list.component.html',
+  styleUrls: ['./admin-verifications-list.component.scss']
+})
+export class AdminVerificationsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
