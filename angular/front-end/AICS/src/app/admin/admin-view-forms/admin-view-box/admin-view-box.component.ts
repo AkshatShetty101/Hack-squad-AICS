c289fb1599744cb3ecf42af3569ff9c778873737
Akshat Shetty <akshatshetty2908@gmail.com>
2018-03-27 01:12:09 +0530

    forms page completed

--- angular/front-end/AICS/src/app/admin/admin-view-forms/admin-view-box/admin-view-box.component.ts
+++ angular/front-end/AICS/src/app/admin/admin-view-forms/admin-view-box/admin-view-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-admin-view-box',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-view-box.component.scss']
 })
 export class AdminViewBoxComponent implements OnInit {
-
+  @Input()cl:string;
+  @Input()form:{ id: string, title: string, division: string, deadline: number };
   constructor() { }
 
   ngOnInit() {
