bd5a0ce95de7b59e78eb5e96a0d9e788054886d2
Nisheet Sinvhal <nisheet1.sinvhal@gmail.com>
2018-03-31 00:41:43 +0530

    Blockchain routes are complete (Hopefully)

--- server/main/src/routes/routes.js
+++ server/main/src/routes/routes.js
@@ -142,17 +142,16 @@ router.post('/forms/submitToGC',
 	require('./forms/submitToGC'),
 	require('./blockchain/submitForm'));
 
-// router.post('/forms/submitToAdmin',
-// 	verifyMiddleware.verifyPerson,
-// 	verifyMiddleware.verifyGC,
-// 	require('./forms/submitToAdmin'),
-// 	require('./blockchain/submitForm'));
+router.post('/forms/submitToAdmin',
+	verifyMiddleware.verifyPerson,
+	verifyMiddleware.verifyGC,
+	require('./forms/submitToAdmin'),
+	require('./blockchain/submitForm'));
 
 router.post('/forms/approveGC',
 	verifyMiddleware.verifyPerson,
 	require('./forms/approveGCAndSubmitToAdmin'),
 	require('./blockchain/approveForm'));
-// require('./blockchain/submitForm'));
 
 router.post('/forms/approveAdmin',
 	verifyMiddleware.verifyPerson,
@@ -161,13 +160,13 @@ router.post('/forms/approveAdmin',
 	require('./blockchain/approveForm'),
 	require('./blockchain/submitForm'));
 
-// router.post('/forms/rejectAdminAndImprove',
-// 	verifyMiddleware.verifyPerson,
-// 	verifyMiddleware.verifyAdmin,
-// 	require('./forms/rejectFormAdmin'),
-// 	require('./blockchain/rejectForm'),
-// 	require('./forms/assignGCAndDeadline'),
-// 	require('./blockchain/assignForm'));
+router.post('/forms/rejectAdminAndImprove',
+	verifyMiddleware.verifyPerson,
+	verifyMiddleware.verifyAdmin,
+	require('./forms/rejectFormAdmin'),
+	require('./blockchain/rejectFormAdmin'),
+	require('./forms/assignGCAndDeadline'),
+	require('./blockchain/assignForm'));
 
 router.post('/forms/approveRA',
 	verifyMiddleware.verifyPerson,
@@ -208,14 +207,6 @@ router.post('/reqAuth/request',
 	verifyMiddleware.verifyPerson,
 	verifyMiddleware.verifyRequestingAuthority,
 	require('./requestingAuth/makeRequest'));
-
-// router.all('/reqAuth',
-// 	verifyMiddleware.verifyRequestingAuthority,
-// 	graphQLHTTP((req, res) => ({ // to be replaced by router.post
-// 		schema: require('./graphql/schemas/requesting_authority'),
-// 		context: { req, res },
-// 		graphiql: process.env.NODE_ENV !== 'production'
-// 	})));
 
 /**
  * Issue Tracker Routes
