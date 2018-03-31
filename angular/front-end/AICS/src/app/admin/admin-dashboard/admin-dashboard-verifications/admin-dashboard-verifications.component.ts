f0d42da628f9c2d64a486c3a439c4edd6f34e058
Mrunmayee Patil <mrunz21@gmail.com>
2018-03-31 08:23:16 +0530

    Charts added

--- angular/front-end/AICS/src/app/admin/admin-dashboard/admin-dashboard-verifications/admin-dashboard-verifications.component.ts
+++ angular/front-end/AICS/src/app/admin/admin-dashboard/admin-dashboard-verifications/admin-dashboard-verifications.component.ts
@@ -16,4 +16,20 @@ export class AdminDashboardVerificationsComponent implements OnInit {
   previewIt(request: { id: string}) {
     this.requestToLoad = request;
   }
+   // Pie
+   public pieChartLabels: string[] = ['Beyond Deadline', 'Within a week', 'Beyond a week'];
+   public pieChartColors: any[] = [
+     { backgroundColor: ['#d50303', '#ffd52b', '#023611'] }
+   ];
+   public pieChartData: number[] = [300, 500, 300];
+   public pieChartType: string = 'pie';
+ 
+   // events
+   public chartClicked(e: any): void {
+     console.log(e);
+   }
+ 
+   public chartHovered(e: any): void {
+     console.log(e);
+   }
 }
