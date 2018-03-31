81d39ca0a74d808c60b795cc6776884ea1d965ab
Adhrit-Shetty <adhritshetty2908@gmail.com>
2018-03-31 08:17:47 +0530

    OCR

--- angular/front-end/AICS/src/app/admin/admin-dashboard/admin-dashboard-repository/admin-dashboard-repository.component.ts
+++ angular/front-end/AICS/src/app/admin/admin-dashboard/admin-dashboard-repository/admin-dashboard-repository.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Ng4FilesStatus, Ng4FilesSelected } from '../../../ng4-files';
+import { HttpService } from '../../../shared/services/http.service';
+declare var saveAs: any;
 
 @Component({
   selector: 'app-admin-dashboard-repository',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard-repository.component.scss']
 })
 export class AdminDashboardRepositoryComponent implements OnInit {
-
-  constructor() { }
+  public selectedFiles: any;
+  selectedFileName = '--selected file name--';
+  constructor(
+    private  http: HttpService
+  ) { }
 
   ngOnInit() {
   }
-
+  public filesSelect(selectedFiles: Ng4FilesSelected): void {
+    console.log(selectedFiles);
+    if (selectedFiles.status !== Ng4FilesStatus.STATUS_SUCCESS) {
+      this.selectedFiles = selectedFiles.status;
+      return;
+      // Hnadle error statuses here
+    }
+    this.selectedFiles = selectedFiles;
+    this.selectedFileName = selectedFiles.files[0].name;
+  }
+  ocr(){
+    const request = {
+      file: this.selectedFiles.files[0]
+    };
+    console.log(request);
+    this.http.ocr(request)
+      .subscribe(
+        (response: any) => {
+          console.log(response);
+        }
+      );
+  }
 }
